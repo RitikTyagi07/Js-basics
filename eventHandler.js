38611cd6f9fe0f2302eb8012488c3b58733dd0a7
officialtyagi <ritik.tyagi@unthinkable.co>
2022-02-23 09:56:26 +0530

    add handler

---
+++ eventHandler.js
@@ -0,0 +1,10 @@
+function imgSizeIncrease(x){
+    x.style.height = "64px" ;
+    x.style.width = "64px" ;
+    
+    }
+    function imgSizeDecrease(x){
+        x.style.height = "32px" ;
+        x.style.width = "32px" ;
+        
+        }
